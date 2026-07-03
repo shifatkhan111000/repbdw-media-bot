98e019b3fe820970b26db6f46dcb189b337d6b94
shifatkhan111000 <shifatappleapple@gmail.com>
2026-07-03 23:25:07 +0600

    Add database.py

--- database.py
+++ database.py
@@ -8,3 +8,19 @@ db = client["repbdw_bot"]
 movies = db["movies"]
 
 print("✅ MongoDB Connected Successfully")
+from pymongo import MongoClient
+from config import MONGO_URI
+
+client = MongoClient(MONGO_URI)
+db = client["repbdw_bot"]
+movies = db["movies"]
+
+def add_movie(name, year, file_id):
+    movies.insert_one({
+        "name": name,
+        "year": year,
+        "file_id": file_id
+    })
+
+def get_movie(name):
+    return movies.find_one({"name": {"$regex": f"^{name}$", "$options": "i"}})
